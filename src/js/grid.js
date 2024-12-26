c89486816f32ea6dad346d317c8aae87145ecdd8
Moose Capital <48196495+MooseCapital@users.noreply.github.com>
2024-12-26 17:06:03 -0600

    grid color working on mouseenter

---
+++ src/js/grid.js
@@ -0,0 +1,36 @@
+import _ from "lodash";
+import {cursorColor} from "./colorPicker.js";
+
+const grid = document.querySelector("#grid");
+const gridLength = _.range(1, 401, 1);
+
+gridLength.map((item, index) => {
+    
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-square");
+    // gridItem.setAttribute("data-id", `${index + 1}`);
+    gridItem.dataset.id = `${index + 1}`;
+    grid.appendChild(gridItem);
+});
+
+grid.addEventListener("click", (event) => {
+    const gridSquare = event.target.closest(".grid-square");
+    gridSquare.style.backgroundColor = cursorColor;
+    console.log(event.target.dataset);
+    
+    
+})
+
+// const gridSquares = document.querySelectorAll(".grid-square");
+/* for (const square of gridSquares) {
+    square.addEventListener("mouseenter", (event) => {
+        event.target.style.backgroundColor = cursorColor
+        console.log(event.target)
+    })
+} */
+
+/* gridSquares.forEach((square) => {
+    square.addEventListener("mouseenter", (event) => {
+        console.log(event)
+    })
+}) */
