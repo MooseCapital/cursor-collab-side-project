b26f44a9337a9204225c424bcc3a4774621e98c9
Moose Capital <48196495+MooseCapital@users.noreply.github.com>
2025-03-11 04:40:40 -0500

    join websocket button working

--- src/components/WebSocketPage.js
+++ src/components/WebSocketPage.js
@@ -1,24 +1,23 @@
 import { createGrid } from "../services/grid.js";
 import { setUserData, myData } from "../services/userData.js";
 import { cursorColors, setCursor, generateCursorColors } from "../services/cursorSetting.js";
-import "../services/cursorEvents.js"
-import {wsSetup} from "../services/websocket.js";
-
-export { WebSocketPage};
-
+import "../services/cursorEvents.js";
+// import {wsSetup} from "../services/websocket.js";
+import {socketioSetup} from "../services/websocket.js";
+export { WebSocketPage };
 
 function WebSocketPage() {
     // console.log("websocket page loaded");
     setTimeout(() => {
         createGrid();
         setUserData();
-        wsSetup();
-        // swatchGrid.innerHTML = getSwatches();
+        socketioSetup();
     }, 0);
-    
+
     return `
         <div id="grid"></div>
-          <button class="joinbtn" id="join-websocket">Join websocket</button>
+          <button class="serverbtn" id="join-websocket">Join websocket</button>
+          <button class="serverbtn" id="leave-websocket">leave websocket</button>
           <div class="latencyContainer">
               <h2 id="latency">0</h2>
               <p>MS Latency</p>
@@ -26,4 +25,3 @@ function WebSocketPage() {
         
     `;
 }
-
--- src/services/websocket.js
+++ src/services/websocket.js
@@ -1,18 +1,53 @@
-import {io} from "socket.io-client";
+import { io } from "socket.io-client";
 
-export {wsSetup}
+export { socketioSetup };
 
-function wsSetup() {
-const socket = io(import.meta.env.VITE_WSSERVER);
-localStorage.debug = 'socket.io-client:socket'
+function socketioSetup() {
+    const socket = io(import.meta.env.VITE_WSSERVER, {
+        autoConnect: false,
+    });
+    localStorage.debug = "socket.io-client:socket";
+    
+    const joinWS = document.querySelector("#join-websocket");
+    const leaveWS = document.querySelector("#leave-websocket");
+    
+    joinWS.addEventListener("click", (e) => {
+        socket.connect();
+        joinWS.disabled = true;
+        leaveWS.disabled = false;
+    })
+    leaveWS.addEventListener("click", (e) => {
+        socket.disconnect();
+        joinWS.disabled = false;
+        leaveWS.disabled = true;
+    });
 
+    setInterval(() => {
+        const start = Date.now();
+        // socket.volatile.emit("latency", Date.now());
+    }, 3000);
 
-setInterval(() => {
-  const start = Date.now();
+    socket.on("connect", () => {
+        console.log("connected:", socket.connected); // true
+    });
 
-  socket.emit("ping", () => {
-      document.querySelector("#latency").textContent = Date.now() - start;
-  });
-}, 3000);
+    socket.on("disconnect", () => {
+        console.log("connected:", socket.connected); // false
+    });
+    socket.on("latency", (data) => {
+        document.querySelector("#latency").textContent = data;
+    });
 
+    socket.on("connect_error", (error) => {
+        if (socket.active) {
+            // temporary failure, the socket will automatically try to reconnect
+        } else {
+            // the connection was denied by the server
+            // in that case, `socket.connect()` must be manually called in order to reconnect
+            console.log(error.message);
+        }
+    });
+    // socket.volatile.emit( /* ... */ );
 }
+
+// }
